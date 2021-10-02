80298b754306722c3a3a17bf6b50b7bb69c09779
Simon Stier <52674635+simontaurus@users.noreply.github.com>
2021-10-02 06:17:14 +0200

    Create AR-Tabs.php

---
+++ includes/AR-Tabs.php
@@ -0,0 +1,18 @@
+
+<?php
+if( !defined( 'MEDIAWIKI' ) ){
+        die( "This is not a valid access point.\n" );
+}
+
+$wgHooks['SkinTemplateNavigation'][] = 'replaceTabs';
+function replaceTabs( &$skin, &$links) {
+        // Add an additional link
+        //$subPath = explode("/", $_SERVER[REQUEST_URI])[1]; #'wiki'
+        //$pageName = str_replace("/".$subPath, "", $_SERVER[REQUEST_URI]);
+        $links['views']['subpage'] = array(
+                'class' => false, // false or 'selected', defines whether the tab should be highlighted
+                'text' => 'Unterseite erstellen', // what the tab says
+                'href' => "https://$_SERVER[HTTP_HOST]/wiki/CreatePage?superpage=".explode("?", str_replace("/wiki/", "", "$_SERV$
+        );
+        return true;
+}
