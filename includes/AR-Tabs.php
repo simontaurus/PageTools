3de457ab073c180ba82b1c319ce724625f85972b
Simontaurus <simon.stier@gmx.de>
2022-02-02 04:13:36 +0000

    improve link generation

--- includes/AR-Tabs.php
+++ includes/AR-Tabs.php
@@ -3,15 +3,24 @@ if( !defined( 'MEDIAWIKI' ) ){
         die( "This is not a valid access point.\n" );
 }
 
-$wgHooks['SkinTemplateNavigation'][] = 'replaceTabs';
-function replaceTabs( &$skin, &$links) {
+//https://www.mediawiki.org/wiki/Manual%3aHooks/SkinTemplateNavigation
+$wgHooks['SkinTemplateNavigation'][] = function ( &$skin, &$links ) {
         // Add an additional link
-        //$subPath = explode("/", $_SERVER[REQUEST_URI])[1]; #'wiki'
-        //$pageName = str_replace("/".$subPath, "", $_SERVER[REQUEST_URI]);
+        //https://stackoverflow.com/questions/18442495/how-to-get-current-page-url-in-mediawiki
         $links['views']['subpage'] = array(
                 'class' => false, // false or 'selected', defines whether the tab should be highlighted
                 'text' => 'Unterseite erstellen', // what the tab says
-                'href' => "https://$_SERVER[HTTP_HOST]/wiki/CreatePage?superpage=".explode("?", str_replace("/wiki/", "", "$_SERVER[REQUEST_URI]"))[0],
+                'href' => "https://$_SERVER[HTTP_HOST]/wiki/CreatePage?superpage=" . $skin->getTitle()->getDBkey(),
         );
+
+	//not placed properly with Skin:Timeless -> use javascript
+	//$request = $skin->getRequest();
+	//$reveal = $request->getText( 'reveal' );
+	//$links['actions']['slide'] = array(
+	//	'class' => ( $reveal == 'true') ? 'selected' : false,
+	//	'text' => "Slideshow",
+	//	'href' => $skin->makeArticleUrlDetails($skin->getTitle()->getFullText(), 'reveal=true' )['href']
+	//);
+
         return true;
-}
+};
